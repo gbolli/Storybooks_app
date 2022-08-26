65fd136d16bad3badb9caf7faa4295012130f2c3
Gord Bolli <gord@bolli.ca>
2022-08-26 14:18:59 -0700

    Setup morgan logging

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const connectDB = require('./config/db')
 
 // Load config
@@ -8,6 +9,11 @@ dotenv.config({ path: './config/config.env' })
 connectDB();
 
 const app = express();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 const PORT = process.env.PORT
 
