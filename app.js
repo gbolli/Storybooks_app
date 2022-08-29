7608957ca4d1a5cd9d617e09fa1c89e660a34b71
Gord Bolli <gord@bolli.ca>
2022-08-29 09:13:43 -0700

    Add google auth with passport and mongoose User schema

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 const app = express();
 
 // Load config
 dotenv.config({ path: './config/config.env' })
+
+// Passport config
+require('./config/passport')(passport)
 
 const PORT = process.env.PORT
 
@@ -27,11 +32,23 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extName: '.hbs'}))
 app.set('view engine', '.hbs')
 
+// Session middleware (before passport)
+app.use(session({
+    secret: 'whateverIwant',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 app.listen(
     PORT,
