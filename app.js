4423a5196b6a5cb6d535ae57fdcd74e5fc3a7dad
Gord Bolli <gord@bolli.ca>
2022-08-29 15:25:22 -0700

    add form for creating stories and setup routes

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 app.listen(
     PORT,
