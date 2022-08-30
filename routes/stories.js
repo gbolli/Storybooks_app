58af5cacfa4351ff5bf98918db8c18d193a03c03
Gord Bolli <gord@bolli.ca>
2022-08-30 11:33:50 -0700

    Create public stories page and route

--- routes/stories.js
+++ routes/stories.js
@@ -22,4 +22,19 @@ router.post('/', ensureAuth, async (req, res) => {
     }
 })
 
+// @desc    Show All Stories
+// @route   GET /stories
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ status: 'public' })
+            .populate('user')
+            .sort({ createdAt: 'desc' })
+            .lean()
+        res.render('stories/index', { stories })
+    } catch (error) {
+        console.error(error)
+        res.render('error/500')
+    }
+})
+
 module.exports = router
