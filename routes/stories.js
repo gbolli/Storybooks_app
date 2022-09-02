ec01228cf186d8ce8bba0c591b7ae7f4eb350eb6
Gord Bolli <gord@bolli.ca>
2022-09-02 10:52:36 -0700

    Add delete button to dashboard and delete route to remove story from database

--- routes/stories.js
+++ routes/stories.js
@@ -76,4 +76,16 @@ router.put('/:id', ensureAuth, async (req, res) => {
     }
 })
 
+// @desc    Delete Story
+// @route   DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        await Story.remove( { _id: req.params.id })
+        res.redirect('/dashboard')
+    } catch (error) {
+        console.error(error)
+        res.render('error/500')
+    }
+})
+
 module.exports = router
